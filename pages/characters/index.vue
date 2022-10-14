ae70fc896038e0869ae0104350b566bf209f9af4
Andreas Rasper <andreas.rasper@1000grad.de>
2022-10-14 16:43:26 +0200

    nn

---
+++ pages/characters/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        List of characters:
+        <ol>
+            <li 
+                v-for="character in characters.results"
+            >
+                <NuxtLink :to="`/characters/${character.id}`">
+                    {{character.id}} - {{character.name}}
+                </NuxtLink>
+            </li>
+        </ol>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+const { data: characters } = await useFetch('https://rickandmortyapi.com/api/character', { pick: ['results'] })
+</script>   
